1554c7cf442855ac8058b85230e9ce1908ef5974
Andrew Robles <me@andrewrobles.com>
2021-04-01 05:54:05 -0700

    Pass TestGetDecimalDigits

--- formatting/formatting.py
+++ formatting/formatting.py
@@ -3,8 +3,27 @@ def encode(input_text):
 
     return ascii_number
 
-def _get_decimal_digits(input_number):
-    pass
+def _get_decimal_digits(number):
+    digits = []
+
+    while True:
+        curr_digit = number % 10
+        digits.insert(0, curr_digit)
+        number = number - curr_digit
+
+        if number == 0:
+            break
+
+        number = number // 10 
+        
+
+    return digits
+
+
+
+    
+
+
 
 result = encode('A')
 print(result)
