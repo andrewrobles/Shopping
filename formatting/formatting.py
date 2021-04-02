36d10931b5c65da128e5525406c0947c29c105a4
andrewrobles <andrewroblesdev@gmail.com>
2021-04-02 09:22:05 -0700

    Pass test_get_decimal

--- formatting/formatting.py
+++ formatting/formatting.py
@@ -40,8 +40,8 @@ def get_arr_value(arr, row, col):
 def _get_decimal_value(binary_digits):
     decimal_value = 0
 
-    for i in range(1, len(binary_digits)):
-        binary_index = len(binary_digits) - i 
+    for i in range(len(binary_digits)):
+        binary_index = len(binary_digits) - i - 1
         binary_digit = binary_digits[binary_index]
 
         decimal_value += binary_digit * 2 ** i
