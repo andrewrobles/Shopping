c824baa93a45dd921ed4fece06f99bffca4fd337
andrewrobles <andrewroblesdev@gmail.com>
2021-04-09 12:08:57 -0700

    Remove unused function import

--- formatting/__init__.py
+++ formatting/__init__.py
@@ -1,1 +1,1 @@
-from .formatting import encode, _get_ascii_digits, _get_decimal_digits, _get_binary_digits, _scramble_digits, _get_decimal_value 
+from .formatting import encode, _get_ascii_digits, _get_binary_digits, _scramble_digits, _get_decimal_value
