bb30792f2d19c3adaa2916d85d24f03f9193c290
andrewrobles <andrewroblesdev@gmail.com>
2021-10-18 06:58:55 -0700

    Write first cypress test

--- client/cypress/integration/init.spec.js
+++ client/cypress/integration/init.spec.js
@@ -6,4 +6,8 @@ describe('Cypress', () => {
     it('opens the app', () => {   
         cy.visit('http://localhost:3000') 
     })
+
+    it('displays add your first item button when no items have been added yet', () => {
+        cy.get('.blue-button').should('exist')
+    })
 })
